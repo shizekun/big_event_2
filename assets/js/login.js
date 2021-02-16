2376d18fd206c90d4ae49a2d032bc512fba02302
szk <1484851205@qq.com>
2021-02-16 22:15:33 +0800

    完成了登录页和首页的功能

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,64 +1,48 @@
-$(function() {
-    // 从 layui 中提取 form 表单模块
+$(function () {
+    // 从layui 中提取form表单模块、
     const { form, layer } = layui
-
     // 点击链接进行表单切换
-    $('.link a').click(function() {
+    $('.link a').click(function () {
         $('.layui-form').toggle()
     })
-
     // 校验表单项
     form.verify({
         pass: [
             /^\w{6,12}$/,
             '密码只能在6到12位之间'
-        ],
-        samePass: function(value) {
+        ], samePass: function (value) {
             if (value !== $('#pass').val()) {
-                return '两次面输入不一致'
+                return '两次输入不一致'
             }
         }
     })
-
     // 实现注册功能
-    $('.reg-form').submit(function(e) {
-        // 阻止默认提交
+    $('.reg-form').submit(function (e) {
         e.preventDefault()
-
-        // 发送 ajax
+        // 发送ajax
         axios.post('/api/reguser', $(this).serialize())
             .then(res => {
                 console.log(res);
-                // 校验失败
                 if (res.status !== 0) {
-                    return layer.msg('注册失败！')
+                    return layer.msg('注册失败')
                 }
                 // 成功跳转到登录
-                layer.msg('注册成功！')
-                $('.login-form a').click()
+                layer.msg('注册成功')
+                $('.layui-form a').click()
             })
     })
-
     // 实现登录功能
-    $('.login-form').submit(function(e) {
-        // 阻止默认提交
+    $('.login-form').submit(function (e) {
         e.preventDefault()
-
-        // 发送 ajax
         axios.post('/api/login', $(this).serialize())
             .then(res => {
                 console.log(res);
-                // 校验失败
                 if (res.status !== 0) {
-                    return layer.msg('登录失败！')
+                    return layer.msg('登录失败')
                 }
-                // 成功，首先把 token 保存到本地
-                localStorage.setItem('token', res.token)
-                    // 提示登录成功
-                layer.msg('登录成功！')
-                    // 跳转到首页
                 location.href = './index.html'
             })
     })
+
 
 })
